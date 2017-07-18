b815744df7962b58d658c50a30f6615b91f4c6d2
Jiaxiao Zhou <jiz417@ucsd.edu>
2017-07-18 00:23:55 -0700

    redesigned the ui

--- new_code/ui/main_page.py
+++ new_code/ui/main_page.py
@@ -16,6 +16,7 @@ class MainPage(tk.Frame):
         self.tree_view_frame = TreeView(master, self._connector)
         self.bubbles = Visualized(master, self._connector)
         self.parsed = False
+        self.parse_progress = None
         self.pages = []
         self.create_widgets()
 
@@ -26,6 +27,7 @@ class MainPage(tk.Frame):
         _enter_frame = tk.Frame(self)
         _enter_frame.pack(fill=tk.BOTH, expand=True)
 
+        self.parse_progress = ttk.Progressbar(_enter_frame, orient='horizontal', length=200, mode='determinate')
         grab_course_btn = tk.Button(_enter_frame, text=Strings.GET_COUR_BTN_STR, command=self.start_thread)
 
         tk.Button(_enter_frame, text=Strings.TREE_VIEW_BTN_STR, command=self.open_tree_view)\
@@ -34,12 +36,14 @@ class MainPage(tk.Frame):
             .grid(row=0, column=1, padx=10, pady=10)
         tk.Button(_enter_frame, text=Strings.COURSE_PLANNER_BTN_STR, command=self.open_course_planner) \
             .grid(row=0, column=2, padx=10, pady=10)
-        grab_course_btn.grid(row=0, column=4, pady=10, padx=10)
 
+        grab_course_btn.grid(row=0, column=4, pady=10, padx=10)
+        self.parse_progress.grid(row=0, column=5, pady=10, padx=10)
         _enter_frame.columnconfigure(3, weight=4)
         _enter_frame.columnconfigure(8, weight=4)
 
         ttk.Separator(_enter_frame).grid(row=1, column=0, columnspan=10, sticky=tk.EW)
+        self.parse_progress['value'] = 0
 
     def make_widges(self, page):
         page.create_widgets()
@@ -63,11 +67,22 @@ class MainPage(tk.Frame):
 
     def start_thread(self):
         """ Multi-Threading implementation of parsing courses"""
-        if self.parsed:  # to check if all courses have already been parsed
+        if self.parsed:
+            messagebox.showinfo('Already Parsed', 'You have Already Parsed the courses!')
             return
-        parse_course_thread = ParseCourseThread(self._connector)
-        if parse_course_thread.start():
+        self.parse_course_thread = ParseCourseThread(self._connector)
+        self.parse_course_thread.start()
+
+        self.periodiccall()
+
+    def periodiccall(self):
+        self.parse_progress.step(0.25)
+        if self.parse_course_thread.is_alive():
+            self.after(100, self.periodiccall)
+        else:
+            self.parse_progress['value'] = 100
             self.parsed = True
+
 
 class ParseCourseThread(threading.Thread):
     def __init__(self, connector):
@@ -82,15 +97,15 @@ class ParseCourseThread(threading.Thread):
             if not self._connector.parse_courses(link):
                 messagebox.showerror('Error', ConnectionError)
                 exit()
-                return False
         print("Parsing success!")
         print("---start parsing prerequisites and postrequisites---")
 
         self._connector.parse_prerequisites()
         self._connector.parse_postrequsites()
+
         print("Parsing success!")
         messagebox.showinfo('Parsing Success!', 'The courses have been parsed successfully!')
         exit()
-        return True
+
 
 
